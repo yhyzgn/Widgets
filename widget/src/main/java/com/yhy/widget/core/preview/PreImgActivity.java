c969465f4bc4984d8e5ea6e00735c4457c6635aa
颜洪毅 <yhyzgn@gmail.com>
2022-10-26 01:43:16 +0800

    更换视频播放器

--- widget/src/main/java/com/yhy/widget/core/preview/PreImgActivity.java
+++ widget/src/main/java/com/yhy/widget/core/preview/PreImgActivity.java
@@ -128,7 +128,9 @@ public class PreImgActivity extends AppCompatActivity implements ViewTreeObserve
         // 状态栏隐藏
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         ActionBar actionBar = getActionBar();
         if (null != actionBar) {
             actionBar.hide();
